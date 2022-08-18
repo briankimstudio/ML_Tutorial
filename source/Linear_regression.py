b554498775cfb1c586a3a08c5f1e29fd5adffd75
Brian Kim <researchclock@gmail.com>
2022-08-18 11:20:47 +0800

    Linear_regression

--- source/Linear_regression.py
+++ source/Linear_regression.py
@@ -3,12 +3,15 @@
 # Linear Regression Classifier
 #
 
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 import pandas as pd
-from sklearn import metrics
+import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 
 #
 # To generate consistant experimental results
@@ -21,11 +24,11 @@ np.random.seed(0)
 #
 ###############################################################################
 
-X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-y = np.dot(X, np.array([1, 2])) + 3
+raw_dataset = datasets.load_diabetes()
 
-my_dataset = pd.DataFrame(X)
-my_dataset['Target'] = y
+# Create pandas dataframe
+my_dataset = pd.DataFrame(raw_dataset.data, columns=raw_dataset.feature_names)
+my_dataset['class'] = raw_dataset.target
 
 #
 # Check data structure, # of rows, # of columns
@@ -39,6 +42,29 @@ my_dataset.info()
 print('\nData value\n')
 print(my_dataset.head())
 
+#
+# Descriptive statistics
+#
+print('\nDescriptive statistics\n')
+print(my_dataset.describe())
+
+plt.subplots(figsize=(7,7))
+sns.heatmap(my_dataset.corr(), cmap='RdYlBu', annot=True)
+plt.show()
+
+#
+#
+#
+X = my_dataset.drop('class',axis=1)
+y = my_dataset['class']
+
+
+#
+# Split dataset into traning(X_train, y_train) set and test set(X_test, y_test).
+#
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+print(f'\nTraining set : {X_train.shape}, Test set : {y_train.shape}\n')
+
 clf = LinearRegression()
 
 ###############################################################################
@@ -49,10 +75,8 @@ clf = LinearRegression()
 
 print('\nTraining...\n')
 clf.fit(X, y)
-print(clf.score(X, y))
-
-print('\nCoefficient')
-print(clf.coef_)
+print(f'Training score : {clf.score(X_train, y_train)}')
+print(f'Coefficient : {clf.coef_}')
 
 ###############################################################################
 #
@@ -61,9 +85,7 @@ print(clf.coef_)
 ###############################################################################
 
 print('\nPredicting...\n')
-pred = clf.predict(np.array([[3, 5]]))
-
-print(pred)
+y_pred = clf.predict(X_test)
 
 ###############################################################################
 #
@@ -76,6 +98,17 @@ print(pred)
 # disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels = clf.classes_)
 # disp.plot()
 # plt.show()
+
+# plt.scatter(X_test, y_test, color="black")
+# plt.plot(X_test, y_pred, linewidth=2)
+
+# plt.xticks(())
+# plt.yticks(())
+
+# plt.show()
+
+print(f'MSE : {mean_squared_error(y_test, y_pred)}')
+print(f'R2  : {r2_score(y_test, y_pred)}')
 
 # print(f'\nAccuracy  : {metrics.accuracy_score(y_test,y_pred)}')
 # print(f'Precision : {metrics.precision_score(y_test, y_pred)}')
